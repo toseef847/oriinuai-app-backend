456a7900c05e7e2e6755c45cef59902c77f0df42
Toseef Hasan <toseefhasan@gmail.com>
2026-06-16 18:50:46 +0500

    chore: set CORS credentials to false

--- app/main.py
+++ app/main.py
@@ -53,7 +53,7 @@ async def validation_exception_handler(request: Request, exc: RequestValidationE
 app.add_middleware(
     CORSMiddleware,
     allow_origins=settings.allowed_origins_list,
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
